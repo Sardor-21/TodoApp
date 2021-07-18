9499aca4a67bda8bb9477461eac5f468768336e2
Sardor <sardorsafarov090@gmail.com>
2021-07-18 12:44:12 +0500

    master

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import { FaSun } from 'react-icons/fa'
 
 export default class Header extends Component {
+  
     render() {
         return (
             <div className="header d-flex justify-content-between align-items-center text-white">
-                <h1 className="mb-0 fw-bold">T O D O</h1>
-                <button className="btn text-white"><FaSun /></button>
+              {this.props.children}
             </div>
         )
     }
